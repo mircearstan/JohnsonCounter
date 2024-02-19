4522740928d0ebd2182184d15488c62cf85a4bf6
Mircea R. Stan <mircea@virginia.edu>
2024-02-19 16:39:01 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -16,8 +16,23 @@ module tt_um_mircearstan_JohnsonCounter (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+parameter size=7;
+//input clk;
+//input r;
+//output [0:size]out;
+
+reg [0:size]out;
+
+    always @ (posedge clk or negedge rst_n)
+	begin
+        if (~rst_n) 
+			out= 8'b0000_0000;
+		else
+			out={~out[size],out[0:size-1]};   
+	end
+
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out  = out;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
